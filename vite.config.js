93b00416f6fa0a2d2bf1f5f1dbdddd96f0b0008a
BadolJnU <kbadol135@gmail.com>
2025-08-28 09:21:01 -0600

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.JPG'], // Include .JPG files as assets
+  assetsInclude: ['**/*.JPG'],
+  //base: "/My_portfolio" // Include .JPG files as assets
 })
